c0a854252ce93e69ae10f6cd74bcdf5a1649d397
Flambrew <matherne.a4@gmail.com>
2023-09-15 09:28:53 -0400

    improved echo

--- src/echo.c
+++ src/echo.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 main() {
-	int c;
+	unsigned int c, n = 0, l = 0;
 	while ((c = getchar()) != EOF) {
-		putchar(c);
+		if (c == '\n') { ++l; }
+		++n;
+		if (c == '\t') { c = '>'; }
+		if (c == '\n') { c = '<'; }
+  		putchar(c);
 	}
+	printf("%d characters echoed (%d lines)\n", n, l);
 }
+
