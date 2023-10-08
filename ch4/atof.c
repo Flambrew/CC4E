2d6ae17c6956308abc40fae6d2c5f637b7643f18
Flambrew <matherne.a4@gmail.com>
2023-10-08 00:55:50 -0400

    goin to bed last commit

--- ch4/atof.c
+++ ch4/atof.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
-#include <math.h>
 
 main() {
-
+    double atof();
+    printf("1234.5678e9\n");
+    printf("%.5f\n", atof("1234.5678e9"));
 }
 
 double atof(s) char s[]; {
-    int i, c, decimal, exp;
-    double out;
+    unsigned int i, c, decimal, exp;
+    double out, power();
 
     for (i = decimal = exp = 0; (c = s[i]) != '\0'; ++i) {
         if (c - 48 < 10) {
-            if (decimal == 0) {
-                (out *= 10) += c - 48;
+            printf("a\n");
+            if (exp != 0) {
+                decimal = decimal * 10 + c - 48;
+            } else if (decimal == 0) {
+                out = out * 10 + c - 48;
             } else if (decimal != 0) {
-                out += (c - 48) / pow(10, decimal++);
+                out += (c - 48) / power(10, decimal++);
             }
         } else if (c == '.' && decimal == 0) {
+            printf("b\n");
             ++decimal;
         } else if (c == 'e' || c == 'E') {
+            printf("c\n");
+            ++exp;
+        } else { return 0; }
+        printf("%.5f\n\n", out);
+    }
+    return out * power(10, exp);
+}
 
+double power(a, b) double a; int b; {
+    double temp;
+    for (temp = 1; b > 0; --b) {
+        temp *= a;
+    }
+    return temp;
+}
