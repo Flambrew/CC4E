805f96ac9199e63656339e285bef702a1fd91275
Flambrew <matherne.a4@gmail.com>
2023-10-13 08:14:05 -0400

    more ch4 commits

--- ch4/atof.c
+++ ch4/atof.c
@@ -12,7 +12,6 @@ double atof(s) char s[]; {
 
     for (i = decimal = exp = 0; (c = s[i]) != '\0'; ++i) {
         if (c - 48 < 10) {
-            printf("a\n");
             if (exp != 0) {
                 decimal = decimal * 10 + c - 48;
             } else if (decimal == 0) {
@@ -21,18 +20,15 @@ double atof(s) char s[]; {
                 out += (c - 48) / power(10, decimal++);
             }
         } else if (c == '.' && decimal == 0) {
-            printf("b\n");
             ++decimal;
-        } else if (c == 'e' || c == 'E') {
-            printf("c\n");
-            ++exp;
+        } else if (exp == 0 && (c == 'e' || c == 'E')) {
+            decimal = ++exp - 1;
         } else { return 0; }
-        printf("%.5f\n\n", out);
     }
-    return out * power(10, exp);
+    return (out * power(10, decimal));
 }
 
-double power(a, b) double a; int b; {
+double power(a, b) int a, b; {
     double temp;
     for (temp = 1; b > 0; --b) {
         temp *= a;
